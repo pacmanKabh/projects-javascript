73e1174ac6da699f3bd94c8ec4c1ce32ab1ed8c2
pacmanKABH <alanyamobile25@gmail.com>
2022-10-29 12:53:32 -0500

    Mostrando todos los números aleatorios generados

--- number-random/js/script.js
+++ number-random/js/script.js
@@ -1,18 +1,16 @@
 // Diseño html
-let sectionContainer = document.querySelector('.container');
-let numberRandomContainer = document.querySelector('.number_random');
-let numberRandomButton = document.querySelector('button');
+let listNumberRandom = document.querySelector('.listNumberRandom');
+let button = document.querySelector('button');
 
-let numberRandomText = document.createElement('label');
-
-numberRandomButton.textContent = 'Nuevo número aleatorio';
+button.textContent = 'Nuevo número aleatorio';
 
 // Lógica de programación del juego
-// Números aleatorios de 1 hasta 10
+// Números aleatorios de 1 hasta 5
 function numberRandom() {
-    let numberRandom = Math.floor(Math.random() * 10) + 1;
-    numberRandomText.textContent = numberRandom;
-    numberRandomContainer.append(numberRandomText);
+    let numberRandom = Math.floor(Math.random() * 5) + 1;
+    let numberText = document.createElement('label');
+    numberText.textContent = numberRandom;
+    listNumberRandom.appendChild(numberText);
 }
 
-numberRandomButton.addEventListener('click', numberRandom);
+button.addEventListener('click', numberRandom);
